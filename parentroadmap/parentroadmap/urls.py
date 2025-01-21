757df83afd15c8010b4dfdcfa3436ab6c5ede22f
Irek <irek_shaihnurov@mail.ru>
2025-01-21 14:16:31 +0300

    Оптимизирована маршрутизация, теперь urls.py продублирован в папке приложения и все адреса пишем туда + можно будет использовать для вложения адреса

--- parentroadmap/parentroadmap/urls.py
+++ parentroadmap/parentroadmap/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from parentroadmapapp.views import index
+from django.urls import path, include
+from parentroadmapapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', include('parentroadmapapp.urls')),
 ]
