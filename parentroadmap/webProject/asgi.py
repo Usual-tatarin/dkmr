6ad9ac28018825aededc9f73b54446ba713504ae
Shamaich01 <ilikero@mail.ru>
2025-01-26 15:52:16 +0300

    refactor names

--- parentroadmap/webProject/asgi.py
+++ parentroadmap/webProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for parentroadmap project.
+ASGI config for webProject webProject.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parentroadmap.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webProject.settings')
 
 application = get_asgi_application()
--- parentroadmap/webProject/wsgi.py
+++ parentroadmap/webProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for parentroadmap project.
+WSGI config for webProject webProject.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parentroadmap.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webProject.settings')
 
 application = get_wsgi_application()
