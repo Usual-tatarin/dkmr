3f1015c51f03ba0fa3b505e3b3b9c3d9ee96fd46
Irek <irek_shaihnurov@mail.ru>
2025-05-06 10:52:29 +0300

    Всё работает

--- parentroadmap/parentroadmapapp/views.py
+++ parentroadmap/parentroadmapapp/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render
-from .models import Prepregnancy, Menu
+from .models import Prepregnancy, Pregnancy, Parenting, Menu
 
-menu_items = Prepregnancy.objects.values('title', 'slug')
+menu_items_prepregnancy = Prepregnancy.objects.values('title', 'slug')
+menu_items_pregnancy = Pregnancy.objects.values('title', 'slug')
+menu_items_parenting = Parenting.objects.values('title', 'slug')
 menu = Menu.objects.values('title', 'url')
 
 
 def index(request):
     return render(request, 'parentroadmapapp/index.html',
-                  {'title': 'Главная страница', 'menu': menu, 'menu_items': menu_items})
+                  {'title': 'Главная страница', 'menu': menu})
 
 
 def prepregnancy(request):
     return render(request, 'parentroadmapapp/prepregnancy.html',
-                  {'title': 'Хочу стать родителем', 'menu': menu, 'menu_items': menu_items})
+                  {'title': 'Хочу стать родителем', 'menu': menu, 'menu_items': menu_items_prepregnancy})
 
 
 def pregnancy(request):
     return render(request, 'parentroadmapapp/pregnancy.html',
-                  {'title': 'Ведение беременности', 'menu': menu, 'menu_items': menu_items})
+                  {'title': 'Ведение беременности', 'menu': menu, 'menu_items': menu_items_pregnancy})
 
 
 def parenting(request):
     return render(request, 'parentroadmapapp/parenting.html',
-                  {'title': 'Я родитель', 'menu': menu, 'menu_items': menu_items})
+                  {'title': 'Я родитель', 'menu': menu, 'menu_items': menu_items_parenting})
 
 
 def post(request, slug):
     return render(request, 'parentroadmapapp/post.html',
-                  {'title': 'Статья', 'menu': menu, 'menu_items': menu_items})
+                  {'title': 'Статья', 'menu': menu})
